553240e75effdbb7f4a7db6cd43ec96f36a199ef
thekrahels <krahelm@gmail.com>
2026-09-01 16:19:12 -0400

    Working through it

--- Homework 1/homework/half_precision.py
+++ Homework 1/homework/half_precision.py
@@ -18,14 +18,27 @@ class HalfLinear(torch.nn.Linear):
         Feel free to set self.requires_grad_ to False, we will not backpropagate through this layer.
         """
         # TODO: Implement me
-        raise NotImplementedError()
+        # raise NotImplementedError()
+        super().__init__(
+            in_features=in_features,
+            out_features=out_features,
+            bias=bias,
+            dtype=torch.float16,
+        )
+
+        self.requires_grad_(False)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Hint: Use the .to method to cast a tensor to a different dtype (i.e. torch.float16 or x.dtype)
         # The input and output should be of x.dtype = torch.float32
         # TODO: Implement me
-        raise NotImplementedError()
+        #raise NotImplementedError()
+        input_dtype = x.dtype
 
+        x_half = x.to(self.weight.dtype)
+        output = super().forward(x_half)
+
+        return output.to(input_dtype)
 
 class HalfBigNet(torch.nn.Module):
     """
@@ -37,7 +50,14 @@ class HalfBigNet(torch.nn.Module):
         def __init__(self, channels: int):
             super().__init__()
             # TODO: Implement me (feel free to copy and reuse code from bignet.py)
-            raise NotImplementedError()
+            #raise NotImplementedError()
+            self.model = torch.nn.Sequential(
+            HalfLinear(channels, channels),
+            torch.nn.ReLU(),
+            HalfLinear(channels, channels),
+            torch.nn.ReLU(),
+            HalfLinear(channels, channels),
+            )
 
         def forward(self, x: torch.Tensor):
             return self.model(x) + x
